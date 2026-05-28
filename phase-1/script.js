f5b4c2499586018030ba0b343d00eb45058de0cd
Aaron Teshale <aaronteshale138@gmail.com>
2026-05-28 17:03:47 +0300

    Phase-1: initial commit

---
+++ phase-1/script.js
@@ -0,0 +1,18 @@
+// PHASE 1
+// Variables: let , const , var
+
+
+// Data-types 
+
+
+// Primitive vs reference types
+
+// Type coercion
+// Equality: == vs ===
+// Functions (declarations, expressions, arrow functions)
+// Scope and lexical scope
+// Closures
+// Hoisting
+// Execution context
+// Call stack
+// The this keyword
